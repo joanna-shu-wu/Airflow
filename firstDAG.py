8f37dd3db368b108eef8be70fbcb648c00a4352f
Shu Wu <shu.wu@coherent.com>
2022-04-19 07:43:41 -0700

    first commit

---
+++ firstDAG.py
@@ -0,0 +1,7 @@
+# Model 1  \                                                       accurate 
+#           \                                                  y / 
+# Model 2 --->  Choose the best Model --> Meet the threshold?   /
+#           /                                                  n \ 
+# Model 3  /                                                      \  inaccurate
+#
+
