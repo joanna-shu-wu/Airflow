3e4a3c57aad1eb990ec253dc7f68c7ca713291de
Shu Wu <shu.wu@coherent.com>
2022-04-19 08:33:37 -0700

    create choose_best_model and use xcom to pass data

--- firstDAG.py
+++ firstDAG.py
@@ -7,7 +7,18 @@
 
 from airflow import DAG
 from datetime import datetime
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+
+
+from random import randint
+def _training_model():
+    return randint(1,10)
+
+from airflow.operators.bash import BashOperator
+def _choose_best_model():
+    if (best_accuray>8): # How to pass the accuracy from the training model to best_accuray accuracy ? Need to share the data from training_model_X tasks into choose_best_model task. Using xcom
+        return 'accurate' # the task_id
+    return 'inaccurate' # the task_id
 
 # create a DAG instance with context manager WITH. 
 # catchup parameter: only the latest non-trigger DAG will be automatically triggered. It avoids many DAG runs between the start date and current date 
@@ -15,6 +26,35 @@ with DAG("my_dag", start_date=datetime(2021,1,1),
 schedule_interval="@daily",catchup=False) as dag:
     
     training_model_A=PythonOperator(
-        task_id="training_model_A" #each task in the DAG needs to have a unique identifier
+        task_id="training_model_A", #each task in the DAG needs to have a unique identifier
+        python_callable=_training_model
 
+    )
+
+    training_model_B=PythonOperator(
+        task_id="training_model_B", #each task in the DAG needs to have a unique identifier
+        python_callable=_training_model
+
+    )
+
+    training_model_C=PythonOperator(
+        task_id="training_model_C", #each task in the DAG needs to have a unique identifier
+        python_callable=_training_model
+
+    )
+
+    choose_best_model=BranchPythonOperator( # need to share the data from training_model_X tasks into choose_best_model task. Using xcom
+        task_id="choose_best_model",
+        python_callable=_choose_best_model
+
+    )
+
+    accurate =BashOperator(
+        task_id="accurate",
+        bash_command="echo 'accurate'"
+    )
+
+    inaccurate =BashOperator(
+        task_id="inaccurate",
+        bash_command="echo 'inaccurate'"
     )
