301b7d3dad4259c38a117520c6b5928ee2c15bc8
Shu Wu <shu.wu@coherent.com>
2022-04-19 08:41:16 -0700

    update docs

--- firstDAG.py
+++ firstDAG.py
@@ -1,6 +1,6 @@
 # Model 1  \                                                       accurate 
 #           \                                                  y / 
-# Model 2 --->  Choose the best Model --> Meet the threshold?   /
+# Model 2 --->  Choose the best Model ( Meet the threshold? )   /
 #           /                                                  n \ 
 # Model 3  /                                                      \  inaccurate
 #
