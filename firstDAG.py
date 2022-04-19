af5a84e1ed7b59f123829099552a938f8f28c10e
Shu Wu <shu.wu@coherent.com>
2022-04-19 08:17:10 -0700

    create the dag object using context manager

--- firstDAG.py
+++ firstDAG.py
@@ -6,3 +6,9 @@
 #
 
 from airflow import DAG
+from datetime import datetime
+
+# create a DAG instance with context manager WITH. 
+# catchup parameter: only the latest non-trigger DAG will be automatically triggered. It avoids many DAG runs between the start date and current date 
+with DAG("my_dag", start_date=datetime(2021,1,1), 
+schedule_interval="@daily",catchup=False) as dag:
